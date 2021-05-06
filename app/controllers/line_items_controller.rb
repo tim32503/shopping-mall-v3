639b249e256afa5843bf443003f94466755b4dce
Eddie Kao <eddie@5xruby.tw>
2021-05-06 15:10:33 +0800

    add navbar and flash notice

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,7 +1,5 @@
 class LineItemsController < ApplicationController
-  include CurrentCart
   before_action :find_cart, only: [:create]
-  rescue_from ActiveRecord::RecordNotFound, with: :invalid_cart
 
   def create
     product = Product.find(params[:product_id])
@@ -14,9 +12,5 @@ class LineItemsController < ApplicationController
   private
     def line_items_params
       params.require(:line_item).permit(:product_id)
-    end
-
-    def invalid_cart
-      redirect_to root_path, notice: '無效的購物車'
     end
 end
